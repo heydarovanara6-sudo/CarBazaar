fc3562492e1b7a0be71ec0cfc9c037ca04abe49c
heydarovanara6-sudo <heydarovanara6@gmail.com>
2025-12-11 01:38:24 +0400

    i will kms

--- src/app.py
+++ src/app.py
@@ -44,9 +44,9 @@ CLOUDINARY_ENABLED = all([
 
 if CLOUDINARY_ENABLED:
     cloudinary.config(
-        cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
-        api_key=os.environ.get('CLOUDINARY_API_KEY'),
-        api_secret=os.environ.get('CLOUDINARY_API_SECRET')
+        cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME', '').strip(),
+        api_key=os.environ.get('CLOUDINARY_API_KEY', '').strip(),
+        api_secret=os.environ.get('CLOUDINARY_API_SECRET', '').strip()
     )
 
 db = SQLAlchemy(app)
